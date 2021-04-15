914703dcead738302865febeaa2f17e5052d67f8
bdefilippi <bruno.de@alumnos.frm.utn.edu.ar>
2021-04-15 03:49:04 -0300

    Agregar archivos de proyecto.

---
+++ ElBuenSabor/Controllers/ElBuenSaborContext.cs
@@ -0,0 +1,20 @@
+﻿using ElBuenSabor.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ElBuenSabor.Controllers
+{
+    public class ElBuenSaborContext : DbContext
+    {
+        public ElBuenSaborContext(DbContextOptions<ElBuenSaborContext> options)
+            :base(options)
+        {
+
+        }
+
+        public DbSet<ArticuloManufacturado> ArticulosManufacturados { get; set; }
+    }
+}
---
+++ ElBuenSabor/Models/ArticuloManufacturado.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ElBuenSabor.Models
+{
+    public class ArticuloManufacturado
+    {
+        public long Id { get; set; }
+        public int TiempoEstimadoCocina { get; set; }
+        public String Denominacion { get; set; }
+        public double PrecioVenta { get; set; }
+        public String Imagen { get; set; }
+
+    }
+}
